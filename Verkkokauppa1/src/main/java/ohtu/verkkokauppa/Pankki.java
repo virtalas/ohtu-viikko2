894a9c2cc22a0f6e9aee456a641cd7de479d895c
virtalas <virtalas@users.noreply.github.com>
2017-11-10 15:17:19 +0200

    t13

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,8 +1,13 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Pankki implements PankkiInterface {
     private Kirjanpito kirjanpito;
 
+    @Autowired
     public Pankki(Kirjanpito k) {
         kirjanpito = k;
     }
